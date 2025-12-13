6d272ce0ce06a9a91a5ade682cf3957dd56736d0
DucIsHere <220372506+DucIsHere@users.noreply.github.com>
2025-12-13 12:44:53 +0700

    Create IslandPopulator.java

---
+++ src/main/java/com/regenerationforrged/world/worldgen/cell/terrian/populator/IslandPopulator.java
@@ -0,0 +1,12 @@
+package com.regenerationforrged.world.worldgen.cell.terrain.populator;
+
+import com.regenerationforrged.world.worldgen.cell.Cell;
+import com.regenerationforrged.world.worldgen.cell.CellPopulator;
+
+public class IslandPopulator implements CellPopulator {
+
+	@Override
+	public void apply(Cell cell, float x, float z) {
+		
+	}
+}
