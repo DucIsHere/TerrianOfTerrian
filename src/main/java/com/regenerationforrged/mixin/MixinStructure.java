772c59e71345e4db6fe0992ffac96e82a80307ff
DucIsHere <220372506+DucIsHere@users.noreply.github.com>
2025-12-13 21:43:42 +0700

    Refactor MixinStructure for custom biome validation
    Refactored MixinStructure to modify isValidBiome method for custom structure placement validation.

--- src/main/java/com/regenerationforrged/mixin/MixinStructure.java
+++ src/main/java/com/regenerationforrged/mixin/MixinStructure.java
@@ -1,16 +1,34 @@
-package regenerationforrged.mixin;
+package com.regenerationforrged.mixin;
 
-import net.minecraft.world.level.levelgen.structure.StructureFeature;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(StructureFeature.class)
+import net.minecraft.core.Holder;
+import net.minecraft.core.HolderLookup.RegistryLookup;
+import net.minecraft.core.RegistryAccess;
+import net.minecraft.world.level.levelgen.structure.Structure;
+import net.minecraft.world.level.levelgen.structure.Structure.GenerationContext;
+import net.minecraft.world.level.levelgen.structure.Structure.GenerationStub;
+import com.regenerationforrged.registries.RTFRegistries;
+import com.regenerationforrged.world.worldgen.structure.rule.StructureRule;
+
+@Mixin(Structure.class)
 public class MixinStructure {
 
-    @Inject(method = "<init>", at = @At("RETURN"))
-    private void initCustomStructure(CallbackInfo ci) {
-        RegenerationForrgedStructure.applyCustomPlacement(this);
+	@Inject(
+		at = @At("HEAD"), 
+		method = "isValidBiome",
+		cancellable = true
+	)
+    private static void isValidBiome(GenerationStub generationStub, GenerationContext generationContext, CallbackInfoReturnable<Boolean> callback) {
+		RegistryAccess registry = generationContext.registryAccess();
+		RegistryLookup<StructureRule> structureRules = registry.lookupOrThrow(RTFRegistries.STRUCTURE_RULE);
+		for(StructureRule structureRule : structureRules.listElements().map(Holder::value).toList()) {
+			if(!structureRule.test(generationContext.randomState(), generationStub.position())) {
+				callback.setReturnValue(false);
+			}
+		}
     }
 }
