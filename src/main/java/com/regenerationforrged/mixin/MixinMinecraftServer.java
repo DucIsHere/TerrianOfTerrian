a71749b881395d65c2fd599583e0a703be6e9b02
DucIsHere <220372506+DucIsHere@users.noreply.github.com>
2025-12-13 21:45:24 +0700

    Update MixinMinecraftServer.java

--- src/main/java/com/regenerationforrged/mixin/MixinMinecraftServer.java
+++ src/main/java/com/regenerationforrged/mixin/MixinMinecraftServer.java
@@ -1,21 +1,41 @@
-package regenerationforrged.mixin;
+package com.regenerationforrged.mixin;
 
-import net.minecraft.server.MinecraftServer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.biome.Climate;
+import net.minecraft.world.level.levelgen.RandomState;
+import net.minecraft.world.level.storage.ServerLevelData;
+import com.regenerationforrged.data.worldgen.preset.settings.Preset;
+import com.regenerationforrged.registries.RTFRegistries;
+
 @Mixin(MinecraftServer.class)
-public class MixinMinecraftServer {
+class MixinMinecraftServer {
 
-    @Inject(method = "startServer", at = @At("HEAD"))
-    private void initWorldgenPipeline(CallbackInfo ci) {
-        RegenerationForrgedPipeline.initConfig();
-    }
-
-    @Inject(method = "loadlevel", at = @At("TAIL"))
-    private void rgf$SafterWorldLoaded(CallBackInfo ci) {
-        RGFRegistry.finishBootstrap();
+	@Inject(
+		at = @At(
+			value = "INVOKE",
+			target = "Lnet/minecraft/world/level/biome/Climate$Sampler;findSpawnPosition()Lnet/minecraft/core/BlockPos;"
+		),
+		method = "setInitialSpawn"
+	)
+    private static void findSpawnPosition(ServerLevel serverLevel, ServerLevelData serverLevelData, boolean bl, boolean bl2, CallbackInfo callback) {
+		RandomState randomState = serverLevel.getChunkSource().randomState();
+		Climate.Sampler sampler = randomState.sampler();
+		serverLevel.registryAccess().lookup(RTFRegistries.PRESET).flatMap((registry) -> {
+			return registry.get(Preset.KEY);
+		}).ifPresent((preset) -> {
+//			if((Object) randomState instanceof RTFRandomState rtfRandomState && (Object) sampler instanceof RTFClimateSampler rtfClimateSampler) {
+//				BlockPos searchCenter = preset.value().world().properties.spawnType.getSearchCenter(rtfRandomState.generatorContext());
+//				RTFCommon.LOGGER.info(searchCenter);
+////				rtfClimateSampler.setSpawnSearchCenter(searchCenter);
+//			} else {
+//				throw new IllegalStateException();
+//			}
+		});
     }
 }
