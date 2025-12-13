8ab8f5f53f8fbefa71666744fac198c1e341096c
DucIsHere <220372506+DucIsHere@users.noreply.github.com>
2025-12-13 21:51:24 +0700

    Update MixinSurfaceSystem.java

--- src/main/java/com/regenerationforrged/mixin/MixinSurfaceSystem.java
+++ src/main/java/com/regenerationforrged/mixin/MixinSurfaceSystem.java
@@ -1,23 +1,47 @@
 package com.regenerationforrged.mixin;
 
-import net.minecraft.world.level.levelgen.SurfaceSystem;
-import net.minecraft.world.level.levelgen.SurfaceRules;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
+
+import org.spongepowered.asm.mixin.Implements;
+import org.spongepowered.asm.mixin.Interface;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.levelgen.PositionalRandomFactory;
+import net.minecraft.world.level.levelgen.RandomState;
+import net.minecraft.world.level.levelgen.SurfaceSystem;
+import com.regenerationforrged.RegenerationForrged;
+import com.regenerationforrged.world.worldgen.surface.RTFSurfaceSystem;
+import com.regenerationforrged.world.worldgen.surface.rule.StrataRule;
 
 @Mixin(SurfaceSystem.class)
-public class MixinSurfaceSystem {
-
-    @Redirect(method = "applySurfaceRules", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/levelgen/SurfaceRules$Context;apply()V"))
-    private void redirectApplySurfaceRules(SurfaceRules.Context context) {
-        RegenerationForrgedSurface.apply(context);
-    }
-
-    @Inject(method = "buildSurface", at = @At("HEAD"), cancellable = true)
-    private void rgf$customSurface(Context ctx, BlockPos pos, double noise, BlockState state, CallBackInfo ci) {
-        if (RGFSurfaceHandler.apply(ctx, pos, noise)) {
-            ci.cancel();
-        }
-    }
+@Implements(@Interface(iface = RTFSurfaceSystem.class, prefix = RTFCommon.MOD_ID + "$RTFSurfaceSystem$"))
+class MixinSurfaceSystem {
+	private static final ResourceLocation GEOLOGY_RANDOM = RTFCommon.location("geology");
+	private RandomState randomState;
+	private Map<ResourceLocation, List<List<StrataRule.Layer>>> strata;
+	
+	@Inject(
+		at = @At("TAIL"),
+		method = "<init>"
+	)
+    public void SurfaceSystem(RandomState randomState, BlockState blockState, int i, PositionalRandomFactory positionalRandomFactory, CallbackInfo callback) {
+    	this.randomState = randomState;
+    	this.strata = new ConcurrentHashMap<>();
+	}
+	
+	public List<List<StrataRule.Layer>> reterraforged$RTFSurfaceSystem$getOrCreateStrata(ResourceLocation name, Function<RandomSource, List<List<StrataRule.Layer>>> strata) {
+		return this.strata.computeIfAbsent(name, (k) -> {
+			PositionalRandomFactory factory = this.randomState.getOrCreateRandomFactory(GEOLOGY_RANDOM);
+			return strata.apply(factory.fromHashOf(k));
+		});
+	}
 }
