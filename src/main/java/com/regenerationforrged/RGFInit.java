2835d45c8e9df3aff596c182bdd436fe67b7c98d
DucIsHere <220372506+DucIsHere@users.noreply.github.com>
2025-12-20 20:50:59 +0700

    Update RGFInit.java

--- src/main/java/com/regenerationforrged/RGFInit.java
+++ src/main/java/com/regenerationforrged/RGFInit.java
@@ -2,16 +2,6 @@ package com.regenerationforrged;
 
 import net.fabricmc.api.ModInitializer;
 
-import net.minecraft.world.level.levelgen.GeneratorType;
-import net.minecraft.world.level.chunk.ChunkGenerator;
-import net.minecraft.core.Registry;
-import net.minecraft.world.level.biome.Biome;
-
-import net.minecraft.world.level.levelgen.chunk.ChunkGeneratorSettings;
-import net.minecraft.world.level.levelgen.flat.FlatChunkGenerator;
-import net.minecraft.world.level.levelgen.flat.FlatChunkGeneratorConfig;
-import net.minecraft.world.level.levelgen.StructuresConfig;
-
 import java.util.Collections;
 import java.util.Optional;
 
@@ -22,22 +12,8 @@ public class RGFInit implements ModInitializer {
     public static final String MOD_ID = "regenerationforrged";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-    // Custom GeneratorType VOID
-    public static final GeneratorType VOID = new GeneratorType("void") {
-        @Override
-        protected ChunkGenerator getChunkGenerator(Registry<Biome> biomeRegistry,
-                                                   Registry<ChunkGeneratorSettings> chunkGeneratorSettingsRegistry,
-                                                   long seed) {
-            FlatChunkGeneratorConfig config = new FlatChunkGeneratorConfig(
-                    new StructuresConfig(Optional.empty(), Collections.emptyMap()), biomeRegistry);
-            config.updateLayerBlocks();
-            return new FlatChunkGenerator(config);
-        }
-    };
-
-    @Override
+	@Override
     public void onInitialize() {
-		ChunkManager.register();
 
         LOGGER.info("Hello Fabric world!");
     }
