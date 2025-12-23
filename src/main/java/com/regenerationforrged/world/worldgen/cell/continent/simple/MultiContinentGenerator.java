cf497e948cf230a7ab9313d80198a15029289f16
DucIsHere <220372506+DucIsHere@users.noreply.github.com>
2025-12-23 12:05:57 +0700

    Create MultiContinentGenerator.java

---
+++ src/main/java/com/regenerationforrged/world/worldgen/cell/continent/simple/MultiContinentGenerator.java
@@ -0,0 +1,11 @@
+package com.regenerationforrged.world.worldgen.cell.continent.simple;
+
+import com.regenerationforrged.world.worldgen.GeneratorContext;
+import com.regenerationforrged.world.worldgen.util.Seed;
+
+public class MultiContinentGenerator extends ContinentGenerator {
+	
+    public MultiContinentGenerator(Seed seed, GeneratorContext context) {
+        super(seed, context);
+    }
+}
