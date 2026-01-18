325154fecedb090120083d431cde76d3ee89c736
DucIsHere <220372506+DucIsHere@users.noreply.github.com>
2026-01-18 11:42:06 +0700

    Create Blend.java

---
+++ src/main/java/com/regenerationforrged/world/worldgen/biome/Blend.java
@@ -0,0 +1,25 @@
+package com.regenerationforrged.world.worldgen.biome;
+
+public emum Blend implements BiomeParameter {
+  LOW_BLEND(-1.0F, 0.35F);
+  MID_BLEND(-0.5F, 0.75F);
+  HIGH_BLEND(0.75F, 1.5F);
+
+  private final float min;
+  private final float max;
+
+  private final Blend(float min, float max) {
+    this.min = min;
+    this.max = max;
+  }
+
+  @Override
+  public float min() {
+    return this.min;
+  }
+
+  @Override
+  public float max() {
+    return this.max;
+  }
+}
