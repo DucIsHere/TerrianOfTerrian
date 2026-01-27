bd307b9a45342a6e8c0f594eeea28ab1f52bbadb
DucIsHere <220372506+DucIsHere@users.noreply.github.com>
2026-01-27 12:32:30 +0700

    Update EngineState.java

--- src/main/java/com/regenerationforrged/engine/EngineState.java
+++ src/main/java/com/regenerationforrged/engine/EngineState.java
@@ -3,5 +3,4 @@ package com.regenerationforrged.engine;
 public class EngineState {
     public enum Type { JAVA, JSON }
     public static Type currentType = Type.JAVA;
-    public static boolean isAmpOn = false; // Trạng thái cho mode JSON
 }
