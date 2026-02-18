25a754ec125012bcc52e0345cffe07a9ddd6d9ef
DucIsHere <220372506+DucIsHere@users.noreply.github.com>
2026-02-18 22:05:17 +0700

    Update Terrian.kt

--- src2/main/kotlin/com/mbz/Terrian.kt
+++ src2/main/kotlin/com/mbz/Terrian.kt
@@ -5,24 +5,24 @@ import net.minecraft.resources.ResourceLocation
 
 import com.mbz.data.worldgen.preset.settings.Preset
 import com.mbz.platform.RegistryUtil
-import com.mbz.registries.RGFBuiltInRegistries
-import com.mbz.registries.RGFRegistries
+import com.mbz.registries.MBZBuiltInRegistries
+import com.mbz.registries.MBZRegistries
 import com.mbz.world.worldgen.biome.modifier.BiomeModifiers
-import com.mbz.world.worldgen.densityfunction.RHFDensityFunctions
-import com.mbz.world.worldgen.feature.RHFFeatures
-import com.mbz.world.worldgen.feature.chance.RGFChanceModifiers
-import com.mbz.world.worldgen.feature.placement.RGFPlacementModifiers
+import com.mbz.world.worldgen.densityfunction.MBZDensityFunctions
+import com.mbz.world.worldgen.feature.MBZFeatures
+import com.mbz.world.worldgen.feature.chance.MBZChanceModifiers
+import com.mbz.world.worldgen.feature.placement.MBZPlacementModifiers
 import com.mbz.world.worldgen.feature.template.decorator.TemplateDecorators
 import com.mbz.world.worldgen.feature.template.placement.TemplatePlacements
-import com.mbz.world.worldgen.floatproviders.RGFFloatProviderTypes
-import com.mbz.world.worldgen.heightproviders.RHFHeightProviderTypes
+import com.mbz.world.worldgen.floatproviders.MBZFloatProviderTypes
+import com.mbz.world.worldgen.heightproviders.MBZHeightProviderTypes
 import com.mbz.world.worldgen.noise.domain.Domains
 import com.mbz.world.worldgen.noise.function.CurveFunctions
 import com.mbz.world.worldgen.noise.module.Noise
 import com.mbz.world.worldgen.noise.module.Noises
 import com.mbz.world.worldgen.structure.rule.StructureRule
 import com.mbz.world.worldgen.structure.rule.StructureRules
-import com.mbz.world.worldgen.surface.rule.RGFSurfaceRules
+import com.mbz.world.worldgen.surface.rule.MBZSurfaceRules
 
 object Terrian {
     const val MOD_ID = "mbz"
@@ -30,26 +30,26 @@ object Terrian {
 
     @JvmStatic
     fun mbz() {
-        RGFBuiltInRegistries.mbz()
+        MBZBuiltInRegistries.mbz()
         TemplatePlacements.mbz()
         TemplateDecorators.mbz()
-        RGFChanceModifiers.mbz()
-        RGFPlacementModifiers.mbz()
-        RGFDensityFunctions.mbz()
+        MBZChanceModifiers.mbz()
+        MBZPlacementModifiers.mbz()
+        MBZDensityFunctions.mbz()
         Noises.mbz()
         Domains.mbz()
         CurveFunctions.mbz()
-        RHFFeatures.mbz()
-        RHFHeightProviderTypes.mbz()
-        RGFFloatProviderTypes.mbz()
+        MBZFeatures.mbz()
+        MBZHeightProviderTypes.mbz()
+        MBZFloatProviderTypes.mbz()
         BiomeModifiers.mbz()
-        RGFSurfaceRules.mbz()
+        MBZSurfaceRules.mbz()
         StructureRules.mbz()
         
         // Dang ky Registry
-        RegistryUtil.createDataRegistry(RGFRegistries.NOISE, Noise.DIRECT_CODEC)
-        RegistryUtil.createDataRegistry(RGFRegistries.PRESET, Preset.DIRECT_CODEC)
-        RegistryUtil.createDataRegistry(RGFRegistries.STRUCTURE_RULE, StructureRule.DIRECT_CODEC)
+        RegistryUtil.createDataRegistry(MBZRegistries.NOISE, Noise.DIRECT_CODEC)
+        RegistryUtil.createDataRegistry(MBZRegistries.PRESET, Preset.DIRECT_CODEC)
+        RegistryUtil.createDataRegistry(MBZRegistries.STRUCTURE_RULE, StructureRule.DIRECT_CODEC)
 
     }
 
