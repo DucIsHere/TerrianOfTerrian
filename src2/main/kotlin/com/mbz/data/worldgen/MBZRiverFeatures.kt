8880225e98745e92b2686c85a7833dde630df44d
DucIsHere <220372506+DucIsHere@users.noreply.github.com>
2026-03-31 13:40:12 +0000

    :))))

---
+++ src2/main/kotlin/com/mbz/data/worldgen/MBZRiverFeatures.kt
@@ -0,0 +1,10 @@
+package com.mbz.data.worldgen
+
+import com.mojang.serilization.Codec
+import net.minecraft.core.BlockPos
+import net.minecraft.world.level.Blocks
+import net.minecraft.world.level.chunk.ChunkAccess
+import net.minecraft.world.level.levelgen.feature.Feature
+import net.munecraft.world.level.levelgen.feature.FeaturePlaceContext
+
+object MBZRiverFeatures
