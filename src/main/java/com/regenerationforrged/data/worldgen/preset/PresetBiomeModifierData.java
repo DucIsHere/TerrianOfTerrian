9cd87e109d9b7d80acf0c519cef46039480a02ee
DucIsHere <220372506+DucIsHere@users.noreply.github.com>
2026-05-03 12:57:36 +0700

    :)))

---
+++ src/main/java/com/regenerationforrged/data/worldgen/preset/PresetBiomeModifierData.java
@@ -0,0 +1,16 @@
+package com.regenerationforrged.data.worldgen.preset;
+
+import java.util.Map;
+
+import net.minecraft.core.Holder;
+import net.minecraft.core.HolderGetter;
+import net.minecraft.core.HolderSet;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.worldgen.BootstapContext;
+import net.minecraft.data.worldgen.placement.VegetationPlacements;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.level.biome.Biomes;
+import net.minecraft.world.level.levelgen.GenerationStep;
+import net.minecraft.world.level.levelgen.placement.PlacedFeature;
+
